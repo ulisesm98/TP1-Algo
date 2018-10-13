d41294ad826cecfaaf41c210257b9730b2230b6c
Eze Centofanti <ezecentofanti@gmail.com>
2018-10-13 01:51:50 -0300

    funciones del read OK, falta validar GGA

--- read_nmea.c
+++ read_nmea.c
@@ -1,50 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
 
-#define STR_HEX "0123456789ABCDEF"
+status_t read_nmea(char (*statement)[],int *checksum) { // statement es un puntero a un string
 
-
-status_t read_nmea(char *statement[]) {
-
-	int c, checksum = 0;
+	int c, i, cksm1, cksm2;
+	*checksum = 0;
 
 	while((c = getchar()) != CHAR_INIT_NMEA && c != EOF);
-	for(i = 0; (c = getchar()) != CHAR_INIT_NMEA && c != '\n' && c != '\0' && c != EOF && c != CHAR_END_NMEA; i++) {
-		statement[i] = c;
-		checksum ^= c;
+	for(i = 0; (c = getchar()) != CHAR_END_NMEA && c != '\n' && c != EOF && c != '\0' && c != CHAR_INIT_NMEA && i < MAX_STR_NMEA; i++) {
+		(*statement)[i] = c; //es necesario el parentesis?
+		*checksum ^= c;
 	}
+	//cuando termina la información util del statement coloca '\0'
+	(*statement)[i] = '\0';
 
 	if(c != CHAR_END_NMEA) {
 		if(c == EOF)
 			return ST_EOF;
+		if(c == CHAR_INIT_NMEA) { // si sale del for porque c = $, pudo haber tomado tal simbolo de la siguiente sentencia, se devuelve y sale.
+			ungetc(CHAR_INIT_NMEA, stdin); // se puede? es feo pero bueno no queda otra.
+			return ST_DATA_ERR;
+		}
+
 		return ST_DATA_ERR; //HOLA VAGO ################################################## HOLA PATO ME OLVIDE DE BORRAR
 	}
 
-	i++;
+	//llega al '*' lee los dos caracteres del checksum y si son "validos" los pasa a int con hexstring_2_integer
+	if((cksm1 = getchar()) == EOF || cksm1 == '\0' || (cksm2 = getchar()) == EOF || cksm2 == '\0')
+		return ST_DATA_ERR;
+	if(hexstring_2_integer((char)cksm1,(char)cksm2) != *checksum)
+		return ST_DATA_ERR;
 
-
-
-	
-
-	
-	
+	return ST_OK;
 }
 
-int hexstring_2_integer(char d1, char d2) {
 
-	int hex1, hex2;
-	char hexa_dic[] = STR_HEX;
-	size_t i;
-	char *ptr;
 
+
+int hexstring_2_integer(int d1, int d2) {
+
+	size_t hex1, hex2;
+	char hexa_dic[] = {STR_HEX}, *ptr;
+
+	//busca el primer caracter en el dicc, si no lo encuentra --> error
 	if((ptr = strrchr(hexa_dic, d1)) == NULL)
-		return -1; //HC!!!
-
-	hex1 = (int)ptr - hexa_dic; //consultar
-
-	return hex1*16 + hex2;
-
-
-
-
-
+		return 2;//HEXSTRING_NULL_FIND_INT;
+	//calcula la posicion del caracter respecto al comienzo de la cadena
+	hex1 = (size_t)ptr - (size_t)hexa_dic; //consultar
+	
+	if((ptr = strrchr(hexa_dic, d2)) == NULL)
+		return 3;//HEXSTRING_NULL_FIND_INT;
+	hex2 = (size_t)ptr - (size_t)hexa_dic; //consultar
+	
+	if(hex1 >= 16 || hex2 >= 16) // HC??
+		return 4;//HEXSTRING_NULL_FIND_INT;
+	return (int)hex1 * 16 + hex2; // HC ??
 
 }   // si está mal devuelve -1
--- read_nmea.h
+++ read_nmea.h
@@ -1,2 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
 #define CHAR_INIT_NMEA '$'
 #define CHAR_END_NMEA '*'
+#define MAX_STR_NMEA 70
+#define STR_HEX "0123456789ABCDEF"
+#define HEXSTRING_NULL_FIND_INT -1
+
+typedef enum {ST_DAT_ERR, ST_OK, ST_HELP, ST_EOF} status_t;
+typedef enum {INVALID_FIX, GPS_FIX, DGPS_FIX, PPS_FIX, RTK_FIX, FLOAT_RTK_FIX, ESTIMATED_FIX, MANUAL_FIX, SIMULATION_FIX} fix_quality;
+
+typedef struct trackpt {
+	struct tm trackpt_time;
+	double latitud;
+	double longitud;
+	fix_quality quality;
+	size_t n_sat;
+	double hdop;
+	double elevation;
+	double height_of_geoid;
+} trackpt_t;
+
+
+status_t read_nmea(char (*statement)[]);
+int hexstring_2_integer(int d1, int d2);
