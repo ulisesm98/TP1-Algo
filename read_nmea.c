bc7035274afe9bd49d61343c4bc8832af16f0dc9
CentofantiEze <42658822+CentofantiEze@users.noreply.github.com>
2018-10-14 22:15:21 -0300

    Update read_nmea.c

--- read_nmea.c
+++ read_nmea.c
@@ -4,15 +4,15 @@
 #include <ctype.h>
 #include <time.h>
 
-status_t read_nmea(char (*statement)[],int *checksum) { // statement es un puntero a un string
+status_t read_nmea(char (*statement)[]) { //recive la dirección de una cadena, en la cual carga la sentencia
 
 	int c, i, cksm1, cksm2;
-	*checksum = 0;
+	checksum = 0;
 
 	while((c = getchar()) != CHAR_INIT_NMEA && c != EOF);
 	for(i = 0; (c = getchar()) != CHAR_END_NMEA && c != '\n' && c != EOF && c != '\0' && c != CHAR_INIT_NMEA && i < MAX_STR_NMEA; i++) {
-		(*statement)[i] = c; //es necesario el parentesis?
-		*checksum ^= c;
+		(*statement)[i] = c;
+		checksum ^= c;
 	}
 	//cuando termina la información util del statement coloca '\0'
 	(*statement)[i] = '\0';
@@ -31,7 +31,7 @@ status_t read_nmea(char (*statement)[],int *checksum) { // statement es un punte
 	//llega al '*' lee los dos caracteres del checksum y si son "validos" los pasa a int con hexstring_2_integer
 	if((cksm1 = getchar()) == EOF || cksm1 == '\0' || (cksm2 = getchar()) == EOF || cksm2 == '\0')
 		return ST_DATA_ERR;
-	if(hexstring_2_integer((char)cksm1,(char)cksm2) != *checksum)
+	if(hexstring_2_integer((char)cksm1,(char)cksm2) != checksum)
 		return ST_DATA_ERR;
 
 	return ST_OK;
@@ -47,16 +47,16 @@ int hexstring_2_integer(int d1, int d2) {
 
 	//busca el primer caracter en el dicc, si no lo encuentra --> error
 	if((ptr = strrchr(hexa_dic, d1)) == NULL)
-		return 2;//HEXSTRING_NULL_FIND_INT;
+		return HEXSTRING_NULL_FIND_INT;
 	//calcula la posicion del caracter respecto al comienzo de la cadena
 	hex1 = (size_t)ptr - (size_t)hexa_dic; //consultar
 	
 	if((ptr = strrchr(hexa_dic, d2)) == NULL)
-		return 3;//HEXSTRING_NULL_FIND_INT;
+		return HEXSTRING_NULL_FIND_INT;
 	hex2 = (size_t)ptr - (size_t)hexa_dic; //consultar
 	
 	if(hex1 >= 16 || hex2 >= 16) // HC??
-		return 4;//HEXSTRING_NULL_FIND_INT;
+		return HEXSTRING_NULL_FIND_INT;
 	return (int)hex1 * 16 + hex2; // HC ??
 
 }   // si está mal devuelve -1
